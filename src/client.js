ebead8c29eb1083007755c67adbddd72cc617e4d
sonnym <michaud.sonny@gmail.com>
2010-09-21 21:39:26 -0400

    updated jquery-ui, added dialog widget; buggy implementation of pawn promotion

--- src/client.js
+++ src/client.js
@@ -2,23 +2,25 @@ var ib = function() {
     ///////////////////////
    // private variables //
   ///////////////////////
-  var pieces = { "K": "&#9812;"
-               , "Q": "&#9813;"
-               , "R": "&#9814;"
-               , "B": "&#9815;"
-               , "N": "&#9816;"
-               , "P": "&#9817;"
-               , "k": "&#9818;"
-               , "q": "&#9819;"
-               , "r": "&#9820;"
-               , "b": "&#9821;"
-               , "n": "&#9822;"
-               , "p": "&#9823;"
-               , "":  "&nbsp;"
-               }
+  var white_pieces = { "K": "&#9812;"
+                     , "Q": "&#9813;"
+                     , "R": "&#9814;"
+                     , "B": "&#9815;"
+                     , "N": "&#9816;"
+                     , "P": "&#9817;"
+                     }
+  var black_pieces = { "k": "&#9818;"
+                     , "q": "&#9819;"
+                     , "r": "&#9820;"
+                     , "b": "&#9821;"
+                     , "n": "&#9822;"
+                     , "p": "&#9823;"
+                     }
+    , pieces = {}
     , boards = {}
     , show_moves = true
     , flipped = false   // with respect to fen
+    , promotion_piece = ""
     , DEBUG = false;
 
     ////////////////////
@@ -26,6 +28,15 @@ var ib = function() {
   ////////////////////
   return {
     play : function() {
+      // http://snipplr.com/view/10430/jquery-object-keys/ => https://groups.google.com/group/jquery-en/browse_thread/thread/3d35ff16671f87a2%5C
+      $.extend({ keys: function(obj) {
+                   var a = [];
+                   $.each(obj, function(k) { a.push(k) });
+                   return a;
+                 }
+              });
+      $.extend(pieces, black_pieces, white_pieces, {"": "&nbsp;"});
+
       if (DEBUG) {
         var f = document.createElement("script");
         f.setAttribute("type","text/javascript");
@@ -33,16 +44,19 @@ var ib = function() {
         document.getElementsByTagName("head")[0].appendChild(f)
 
         init();
-      } else {
-        $.getScript("board.js", function(data, textStatus) { init(); });
+      } else $.getScript("board.js", function(data, textStatus) { init(); });
+    }
+  , toggle_show_moves : function(sm) {
+        show_moves = sm;
       }
-    },
-    toggle_show_moves : function(sm) {
-      show_moves = sm;
-    },
-    toggle_flip_board : function() {
+  , toggle_flip_board : function() {
       flipped = !flipped;
       draw_board("primary");
+    }
+  , toggle_promotion_piece : function(piece) {
+      if (promotion_piece) $("#promotion_piece" + promotion_piece).removeClass("promotion_piece_selected");
+      $("#promotion_piece" + piece).addClass("promotion_piece_selected");
+      promotion_piece = piece;
     }
   };
 
@@ -55,6 +69,7 @@ var ib = function() {
     $("#welcome").remove();
     draw_board("primary");
   }
+
   function draw_board(b) {
     $("#" + b + " > .board").html(array2board(b));
     $("#" + b + " > .board > .square > .piece").draggable( { revert: "invalid"
@@ -100,18 +115,67 @@ var ib = function() {
   }
 
   function display_moves(board, piece) {
-    var piece_location = parseInt(piece.parent()[0].id.substring(board.length))
-      , valid = boards[board].get_valid_locations(piece_location);
+    var piece_location = get_location_from_piece_div(board, piece)
+      , valid = boards[board].get_valid_locations(piece_location)
+      , turn = get_turn_from_piece_div(piece);
 
     for (var i = 0, l = valid.length; i < l; i++) {
       $("#" + board + valid[i]).droppable({ tolerance: "fit"
                                           , activeClass: (show_moves) ? "droppable" : ""
                                           , hoverClass: "droppable_hover"
                                           , drop: function(event, ui) {
-                                              boards[board].update_state(piece_location, parseInt($(this).attr("id").substring(board.length)));
+                                              boards[board].update_state(piece_location
+                                                                        , parseInt($(this).attr("id").substring(board.length))
+                                                                        , function(message, callback) { if (message == "promote") display_promotion_dialog(turn, callback); }
+                                                                        );
                                               draw_board(board);
                                             }
                                           });
     }
+  }
+
+  function display_promotion_dialog(turn, callback) {
+    $("#promotion").dialog({ autoOpen: true
+                           , closeOnEscape: false
+                           , closeText: ""
+                           , draggable: false
+                           , modal: true
+                           , title: "Select a Piece"
+                           , buttons: { "Ok": function() { $(this).dialog("close"); } }
+                           , open: function(event, ui) {
+                              $(this).html(get_promotion_pieces(turn));
+                              $(this).removeClass("hidden");
+                             }
+                           , beforeClose: function(event, ui) {
+                               if (promotion_piece == "") return false;
+                             }
+                           , close: function(event, ui) {
+                               callback(promotion_piece);
+                               $(this).addClass("hidden");
+                               $(this).dialog("destroy");
+                             }
+                           });
+  }
+
+  function get_promotion_pieces(turn) {
+    var pieces = (turn == "w") ? white_pieces : black_pieces
+      , piece_keys = $.keys(pieces)
+      , ret = "";
+
+    for (var p in piece_keys) {
+      var piece = piece_keys[p];
+      if (piece.toLowerCase() != "p" && piece.toLowerCase() != "k") ret += "<div class=\"promotion_piece\" id=\"promotion_piece" + piece + "\" onclick=\"ib.toggle_promotion_piece('" + piece + "');\">" + pieces[piece] + "</div>";
+    }
+
+    return ret;
+  }
+
+  function get_location_from_piece_div(board, d) {
+    return parseInt(d.parent()[0].id.substring(board.length));
+  }
+
+  function get_turn_from_piece_div(d) {
+    var ascii = d.children().first().html().charCodeAt(0);
+    return (ascii > 64 && ascii < 91) ? "w" : (ascii > 96 && ascii < 123) ? "b" : null;
   }
 }();
--- src/server.js
+++ src/server.js
@@ -16,5 +16,5 @@ handler.get("/board.js", handler.staticHandler("board.js"));
 handler.get("/client.js", handler.staticHandler("client.js"));
 handler.get("/client.css", handler.staticHandler("client.css"));
 handler.get("/lib/jquery-1.4.2.min.js", handler.staticHandler("lib/jquery-1.4.2.min.js"));
-handler.get("/lib/jquery-ui-1.8.4.custom.min.js", handler.staticHandler("lib/jquery-ui-1.8.4.custom.min.js"));
+handler.get("/lib/jquery-ui-1.8.5.custom.min.js", handler.staticHandler("lib/jquery-ui-1.8.5.custom.min.js"));
 handler.get("/lib/awesome-buttons/awesome-buttons.css", handler.staticHandler("lib/awesome-buttons/awesome-buttons.css"));
