844aa50304175f08bf0e59856c96a5ecd092e4d8
sonnym <michaud.sonny@gmail.com>
2010-10-01 17:40:43 -0400

    added node-websocket-client and created test for connecting via websockets

--- test/unit/client.js
+++ test/unit/client.js
@@ -1,10 +1,22 @@
 var assert = require("assert")
   , http = require("http")
-  , client = http.createClient(8124);
+  , sys = require("sys")
+  , WebSocket = require("./../lib/node-websocket-client/lib/websocket").WebSocket
+
+  , client_http = http.createClient(8124)
+  , client_sock = new WebSocket("ws://127.0.0.1:8124/socket.io/websocket", "borf");
 
 exports.can_fetch_index = function() {
-  client.request("/", function(response) {
-    console.log(response.statusCode);
+  var request = client_http.request("GET", "/");
+
+  request.end();
+  request.on("response", function(response) {
     assert.equal(response.statusCode, 200);
+  });
+}
+
+exports.can_connect_via_websocket = function() {
+  client_sock.addListener('data', function(buf) {
+    assert.notEqual(buf.length, 0);
   });
 }
