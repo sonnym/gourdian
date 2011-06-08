028f7e8503bdc4e0475828ff2b82cbe24d101fdd
sonnym <michaud.sonny@gmail.com>
2011-06-08 19:29:04 -0400

    added boilerplate for chunked streaming

--- core/boilerplate/init/app/c/welcome.js
+++ core/boilerplate/init/app/c/welcome.js
@@ -1,8 +1,15 @@
-require("util").inherits(module, require("./../../core/controller.js"));
+util.inherits(module, Controller);
+
 var Welcome = module.exports = function() {
   return {
     index: function() {
       return "Welcome to the Gourdian project!<br>The file you are looking at was created dynamically.";
+    }
+  , streaming: function() {
+      var test = dust.compile(require("fs").readFileSync(require("path").join(Gourdian.ROOT, "app", "v", "welcome", "streaming.dust.html"), "utf8"), "template_name");
+      dust.loadSource(test);
+
+      return dust.stream("template_name", { version: process.version });
     }
   };
 };
