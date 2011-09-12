238bb9b9c3df7fe612a977ca74854be893e42d56
sonnym <michaud.sonny@gmail.com>
2011-09-11 22:20:10 -0400

    fix test causing hang

--- test/unit/script.js
+++ test/unit/script.js
@@ -12,25 +12,27 @@ var ScriptTest = function() {
     });
   }
 
-  this.server_script_runs_and_gets_to_repl_without_stderr = function() {
+  this.server_script_runs_and_gets_to_repl_without_stderr_and_stops = function() {
     var async = this.start()
-      , data = "";
-
-    this.spawner("./script/server.js", [],
-      { "stdout": function(datum) {
-          data += datum;
-          if (data.length >= 6) {
-            data += ""; /* data needs to be manipulated in order for this test to work
-                           presumably because it is buffer but is coerced this way */
-            assert.ok(data.indexOf("gourd>") >= 0);
+      , data = ""
+      , child = this.spawner("./script/server.js", [],
+        { "stdout": function(datum) {
+            data += datum;
+            if (data.length >= 6) {
+              data += ""; /* data needs to be manipulated in order for this test to work
+                             presumably because it is buffer but is coerced this way */
+              assert.ok(data.indexOf("gourd>") >= 0);
+              child.stdin.write("stop();\n");
+            }
+          }
+        , "stderr": function(datum) {
+            async.message("received stderr: " + datum);
+          }
+        , "exit": function() {
             async.finish();
           }
         }
-      , "stderr": function(datum) {
-          async.message("received stderr: " + datum);
-        }
-      }
-    );
+      );
   }
 }
 
