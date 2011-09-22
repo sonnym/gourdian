be0e19a87455610d4eb796782d9fc2c873ae05af
sonnym <michaud.sonny@gmail.com>
2011-09-22 02:50:49 -0400

    create new async helper method

--- test/performance/socket.js
+++ test/performance/socket.js
@@ -5,18 +5,15 @@ module.exports = SocketTest = function() {
 
   this.server_can_handle_125_sockets = function() {
     var self = this;
-    (function wait() {
-      if (self._clients.length === 125) {
-        // ensure that each client has at least 5 heartbeats after the handshake
-        var ekg = function() {
-          return Gourdian._.reduce(self._clients, function(memo, client) { return (client.heartbeats > 5) }, true);
-        };
-        ext.Sync.wait_for(ekg, function() { async.finish() });
-      } else {
-        self.add_client("ws_connect")
-        setTimeout(wait, null);
-      }
-    })();
+    ext.Sync.wait_for_while(function() { return self._clients.length === 125 }, function() {
+      // ensure that each client has at least 5 heartbeats after the handshake
+      var ekg = function() {
+        return Gourdian._.reduce(self._clients, function(memo, client) { return (client.heartbeats > 5) }, true);
+      };
+      ext.Sync.wait_for(ekg, function() { async.finish() });
+    }, function() {
+      self.add_client("ws_connect")
+    });
   }
 }
 inherits(SocketTest, PerformanceTest);
