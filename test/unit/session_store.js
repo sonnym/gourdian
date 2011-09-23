2ca23dec55fa1786ea1a8e76e5770709615ab06b
sonnym <michaud.sonny@gmail.com>
2011-09-23 07:27:11 -0400

    session store documentation and ability to lookup session object

--- test/unit/session_store.js
+++ test/unit/session_store.js
@@ -6,5 +6,10 @@ module.exports = SessionStoreTest = function() {
   this.generate_key_produces_a_32_byte_string = function() {
     assert.equal(session_store.generate_key().length, 32);
   }
+
+  this.can_get_a_session_after_create = function() {
+    var session_id = session_store.create();
+    assert.ok(session_store.get(session_id));
+  }
 }
 inherits(SessionStoreTest, Test);
