eb03c4545c084b8fbc57c62b1e3c722bf2c2e0f8
sonnym <michaud.sonny@gmail.com>
2011-09-23 07:53:54 -0400

    add ability to search for sessions

--- test/unit/session_store.js
+++ test/unit/session_store.js
@@ -19,5 +19,21 @@ module.exports = SessionStoreTest = function() {
     session_store.destroy(session_id);
     assert.equal(session_store.get(session_id), undefined);
   }
+
+  this.can_search_for_data_saved_in_a_session = function() {
+    var session_id = session_store.create()
+      , session = session_store.get(session_id);
+
+    var secret_key = session_store.generate_key()
+      , secret_val = session_store.generate_key();
+
+    session[secret_key] = secret_val;
+
+    var found = session_store.find(function(val, key) {
+      return (val[secret_key] === secret_val);
+    });
+
+    assert.equal(found, session);
+  }
 }
 inherits(SessionStoreTest, Test);
