27db2c2a7dd494feb38ce03bb5dc5ce0d20ebbc5
sonnym <michaud.sonny@gmail.com>
2011-09-29 06:00:28 -0400

    add examples of alternate styles to test

--- test/fixtures/application/app/c/welcome.js
+++ test/fixtures/application/app/c/welcome.js
@@ -8,5 +8,25 @@ module.exports = WelcomeController = function() {
   this.streaming = function() {
     return { version: process.version };
   }
+
+  /*
+  return {
+    index: function() {
+      return "Welcome to the Gourdian project!<br>The file you are looking at was created dynamically.";
+    }
+    , streaming: function() {
+      return { version: process.version };
+    }
+  };
+  */
 }
 inherits(WelcomeController, Controller);
+
+/*
+WelcomeController.prototype.index = function() {
+  return "Welcome to the Gourdian project!<br>The file you are looking at was created dynamically.";
+}
+WelcomeController.prototype.streaming = function() {
+  return { version: process.version };
+}
+*/
