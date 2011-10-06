6be1ddd2215e8677729867a53b682b7a6d2cc92f
sonnym <michaud.sonny@gmail.com>
2011-10-05 23:59:44 -0400

    consolodate timeouts for slow tests

--- test/unit/bin.js
+++ test/unit/bin.js
@@ -1,9 +1,9 @@
 module.exports = BinTest = function() {
   Test.call(this)
   var async = this.start();
+  this._timeout = 1000;
 
   this.can_initialize_an_empty_project_in_a_nonexistent_directory = function() {
-    this._timeout = 500;
     this.spawner( "gourdian", ["init", path.join("test", "fixtures", "empty_nex")]
                 , { "exit": function(code, sig) {
                        assert.equal(code, 0);
@@ -20,7 +20,6 @@ module.exports = BinTest = function() {
   }
 
   this.can_initialize_an_empty_project_in_an_existing_directory = function() {
-    this._timeout = 1000;
     var self = this;
     fs.mkdir(path.join("test", "fixtures", "empty_ex"), 0777, function(err) {
       self.spawner( "gourdian", ["init", path.join("test", "fixtures", "empty_ex")]
