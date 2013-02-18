cbc8c1a8fbbddf52e859e0eb740f890fb85074ac
sonnym <michaud.sonny@gmail.com>
2013-02-18 14:59:40 -0500

    make server script a gourdian command

--- bin/gourdian.js
+++ bin/gourdian.js
@@ -37,8 +37,39 @@ if (command === "init") {
       if (response === "y") init_new_app(true);
     });
   }
+} else if (command === "server") {
+  require("gourdian");
+  global.stop = function() { process.kill(process.pid, "SIGHUP") };
+
+    /////////////
+   // options //
+  /////////////
+  var opts = new GetOpt();
+  opts.add("logfile", "Set the location of the log file", "", "l", "logfile", GetOpt.REQUIRED_ARGUMENT);
+  opts.add("port", "Port HTTP server and Socket.IO will listen on", "", "p", "port", GetOpt.REQUIRED_ARGUMENT);
+
+  try {
+    opts.parse(process.argv);
+  } catch (e) {
+    console.log(e + "\nGourdian server script usage: \n\n" + opts.help());
+    return;
+  }
+
+  var logfile = (opts.get("logfile")) ? opts.get("logfile") : null
+    , port = (opts.get("port")) ? opts.get("port") : null;
+
+  // global exception handling
+  process.on("uncaughtException", function(error) {
+    process.stdout.write("Caught exception: " + error + "\n" + error.stack);
+  });
+
+  var server = new Server(logfile, port);
+  server.start();
+
+  require("repl").start("gourd> ");
 } else {
-  console.log("Usage: script/gourdian init [directory]");
+  console.log("Usage: gourdian init [directory]");
+  console.log("       gourdian server");
 }
 
   /////////////
--- test/unit/bin.js
+++ test/unit/bin.js
@@ -3,6 +3,8 @@ var path = require("path");
 var exec = require("child_process").exec;
 var spawn = require("child_process").spawn;
 
+var Gourdian = require("./../../lib/gourdian");
+
 exports.can_initialize_an_empty_project_in_a_nonexistent_directory = function(test) {
   var gourdian = spawn("gourdian", ["init", path.join("test", "fixtures", "empty_nex")]);
   gourdian.on("exit", function(code, signal) {
@@ -10,7 +12,7 @@ exports.can_initialize_an_empty_project_in_a_nonexistent_directory = function(te
     test.equal(signal, null);
 
     exec("ls " + path.join("test", "fixtures", "empty_nex", "script") + " | wc -l" , function(_, stdout, _) {
-      test.equal(3, parseInt(stdout));
+      test.equal(2, parseInt(stdout));
 
       exec("rm -rf " + path.join("test", "fixtures", "empty_nex"), function (err, _, stderr) {
         if (!err && !stderr) {
@@ -38,7 +40,7 @@ exports.can_initialize_an_empty_project_in_an_existing_directory = function(test
       test.equal(code, 0);
       test.equal(signal, null);
       exec("ls " + path.join("test", "fixtures", "empty_ex", "script") + " | wc -l" , function(_, stdout, _) {
-        test.equal(3, parseInt(stdout));
+        test.equal(2, parseInt(stdout));
 
         exec("rm -rf " + path.join("test", "fixtures", "empty_ex"), function (err, _, stderr) {
           if (!err && !stderr) {
@@ -47,5 +49,25 @@ exports.can_initialize_an_empty_project_in_an_existing_directory = function(test
         });
       });
     });
+  });
+}
+
+exports.can_start_and_start_server = function(test) {
+  var script_dir = path.join(Gourdian.ROOT, "script");
+  var stdout  = "";
+
+  var server = spawn("gourdian", ["server"]);
+
+  server.stdout.on("data", function(data) {
+    stdout += data.toString();
+    if (stdout.length >= 6) {
+      test.ok(stdout.indexOf("gourd>") >= 0);
+      server.stdin.write("stop();\n");
+    }
+  });
+
+  server.on("exit", function(code, signal) {
+    test.equal(signal, "SIGHUP");
+    test.done();
   });
 }
